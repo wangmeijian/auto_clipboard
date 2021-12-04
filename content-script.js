57b280021136da88b302e45ab894d978446b21be
王美建 <wangmeijian2016@gmail.com>
2021-12-04 12:21:17 +0800

    navigator.clipboard仅在HTTPS下可用

--- content-script.js
+++ content-script.js
@@ -1,23 +1,31 @@
 const i18n = key => chrome.i18n.getMessage(key);
+
 class AutoClipboard {
   constructor(){
     this.message = this._createMessage();
     this._addActionListener();
   }
-  // 复制选中的文本
+  /**
+   * 复制选中的文本
+   * @returns Promise
+   */ 
   _copySelectedText() {
     const selectedText = window.getSelection().toString()
 
     if(!selectedText || selectedText?.length === 0)return Promise.reject();
-    try {
-      navigator.clipboard.writeText(selectedText);
-      return Promise.resolve(selectedText);
-    } catch (err) {
-      console.error(i18n("copyError"), err);
-      return Promise.reject();
+    // 仅在https下可用
+    if (navigator.clipboard && window.isSecureContext) {
+      return navigator.clipboard.writeText(selectedText);
+    } else {
+      return new Promise((resolve, reject) => {
+          document.execCommand('copy') ? resolve() : reject();
+      });
     }
   }
-  // 提示框
+  /**
+   * 提示框
+   * @returns HTMLElement
+   */
   _createMessage(){
     const message = document.createElement('div');
     message.id = 'autoClipboardMessage';
@@ -40,16 +48,20 @@ class AutoClipboard {
     document.body.appendChild(message);
     return message;
   }
-  // 监听事件回调
+  /**
+   * 监听事件回调
+   */
   _handleAction(){
-    this._copySelectedText().then((copySelectedText) => {
+    this._copySelectedText().then(() => {
       this.message.style.display = 'block';
       setTimeout(() => {
         this.message.style.display = 'none';
-      }, 1000)
+      }, 1000) 
     }).catch(() => { }) 
   }
-  // 事件绑定
+  /**
+   * 事件绑定
+   */
   _addActionListener(){
     const events = ['dblclick', 'keyup', 'mouseup', 'unload'];
 
