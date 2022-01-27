d5190ccafe13fbcc9c8058a221761dde98b42306
王美建 <wangmeijian2016@gmail.com>
2022-01-27 15:58:44 +0800

    增加搜索

--- popup.js
+++ popup.js
@@ -2,11 +2,10 @@ const i18n = (key) => chrome.i18n.getMessage(key);
 
 class Popup {
   constructor() {
-    this._STORAGE_KEY = 'auto_clipboard_history';
+    this._STORAGE_KEY = "auto_clipboard_history";
     this._history = [];
-    this._initPage();
-    this._addEventListener();
     this._timer = null;
+    this._initPage();
   }
   /**
    * @desc 生成页面
@@ -28,14 +27,20 @@ class Popup {
     const optionsDOM = document.createElement("div");
     optionsDOM.innerHTML = optionsHTML;
     document.body.appendChild(optionsDOM);
+    this._addEventListener();
   }
   /**
    * @desc 生成历史记录
    * @returns HTMLElement
    */
-  _buildHistoryHTML(){
-    if(this._history.length === 0) return i18n("historyEmpty");
+  _buildHistoryHTML(filterString = '') {
+    if (this._history.length === 0) return i18n("historyEmpty");
+    filterString = filterString.trim();
+
     return this._history
+      .filter((item) => {
+        return filterString.length > 0 ? item.includes(filterString) : item;
+      })
       .map((item, index) => {
         return `<a class="copy_item" href="#">
           <span class="click_target">${item}</span>
@@ -69,7 +74,6 @@ class Popup {
     window.addEventListener(
       "click",
       (e) => {
-        console.log(e.target.className);
         if (e.target.className === "click_target") {
           this._selectText(e.target);
           this._copy();
@@ -82,15 +86,14 @@ class Popup {
       "click",
       (e) => {
         if (e.target.className === "delete_item") {
-          const index = e.target.getAttribute('dindex');
-          if(typeof index === undefined)return;
+          const index = e.target.getAttribute("dindex");
+          if (typeof index === undefined) return;
           this._history.splice(index, 1);
           chrome.storage.sync.set({
-            [this._STORAGE_KEY]: this._history
-          })
+            [this._STORAGE_KEY]: this._history,
+          });
           // 刷新页面
-          const historyHTML = this._buildHistoryHTML();
-          document.querySelector('.copy_history').innerHTML = historyHTML;
+          this._reload();
         }
       },
       false
@@ -103,6 +106,20 @@ class Popup {
         this._copy();
       }
     });
+    // 过滤
+    window.addEventListener("input", (e) => {
+      if (e.target.className === "search_history") {
+        const searchKey = e.target.value.trim();
+        this._reload(searchKey);
+      }
+    });
+  }
+  /**
+   * 刷新页面
+   */
+  _reload(filterString = '') {
+    const historyHTML = this._buildHistoryHTML(filterString);
+    document.querySelector(".copy_history").innerHTML = historyHTML;
   }
   /**
    * @desc 选中要复制的文本
