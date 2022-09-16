667329609ed307d3c3768291967e6fb3d92a0a75
王美建 <>
2022-09-16 14:30:51 +0800

    fix: 移除可能影响性能的代码

--- extension/initialization.js
+++ extension/initialization.js
@@ -10,33 +10,5 @@
     document.head && document.head.appendChild(globalStyle);
   };
 
-  // 监听DOM变化，移除指定属性
-  const removeAttribute = () => {
-    if (!document.body) return;
-    const attributes = [
-      "oncopy",
-      "unselectable",
-      "onmouseup",
-      "selectstart",
-      "dragstart",
-    ];
-    const observer = new MutationObserver((mutations) => {
-      for (const mutation of mutations) {
-        const node = mutation.target;
-
-        if (node instanceof HTMLElement) {
-          attributes.forEach((attr) => node.removeAttribute(attr));
-        }
-      }
-    });
-
-    // 开始监听
-    observer.observe(document.body, {
-      childList: true,
-      subtree: true,
-    });
-  };
-
   setGlobalStylesheet();
-  removeAttribute();
 })();
